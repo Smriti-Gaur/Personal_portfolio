71b6d0e0f7b3bdc7950d21884a8e3e2dfdbd4b3d
Smriti Gaur <smritigaur2105@gmail.com>
2025-01-04 18:46:34 +0530

    add files

---
+++ script.js
@@ -0,0 +1,20 @@
+
+  document.getElementById("home").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    window.location.href ="/home/home.html" 
+  });
+
+  document.getElementById("about").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    window.location.href ="about_me.html" 
+  });
+
+  document.getElementById("project").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    window.location.href ="/projects/projects.html" 
+  });
+  
+  document.getElementById("contact").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    window.location.href ="contact.html" 
+  });
